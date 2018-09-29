021767e1019f15d15e96a8b250fd2433b49ab142
Kazuhiro Sera <seratch@gmail.com>
2018-09-29 10:08:46 +0900

    Using lombok.Builder in src/test/java doesn't work with JDK 11

---
+++ src/test/java/example/Sample.java
@@ -0,0 +1,16 @@
+package example;
+
+import lombok.*;
+
+// ./gradlew test
+// 
+// Execution failed for task ':test'.
+// > failed to read class file /path/to/lombok-jdk11/build/classes/java/test/example/Sample.class
+//
+// The failure doesn't happen when you place the class under src/main/java
+
+@Builder(toBuilder = true)
+public class Sample {
+    private Integer id;
+    private String name;
+}
